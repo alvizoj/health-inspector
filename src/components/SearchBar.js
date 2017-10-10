874d97a874bdd400c7901ef9eb37aac48b3876a9
Joseph Alvizo <alvizoj@uci.edu>
2017-10-09 21:03:45 -0700

    styled search bar, made api call

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'font-awesome/css/font-awesome.css'
+import './SearchBar.css'
 
 export default class SearchBar extends React.Component {
 
@@ -16,21 +18,37 @@ export default class SearchBar extends React.Component {
 		});
 	};
 
+	handleSubmit = (event) => {
+		event.preventDefault()
+		this.props.updateSearchTerm(this.state.search);
+	};
+
 	render() {
 		return (
 			<div className="searchbar-container">
 				<form className="search-form">
-				<input
-					type="search"
-					name="search"
-					value={this.state.search}
-					className="search-input"
-					placeholder="Search Restaurants"
-					onChange = {this.handleSearchChange}
-				/>
+					<input
+						type="search"
+						name="search"
+						value={this.state.search}
+						className="search-input"
+						placeholder="Search Restaurants"
+						onChange = {this.handleSearchChange}
+					/>
+					<button
+						type="submit"
+						className="search-button"
+						onClick={this.handleSubmit}
+					>
+						<i className="search-icon fa fa-search"/>
+					</button>
 				</form>
-
 			</div>
 		)
 	}
-}
+
+};
+
+// PropTypes.PropTypes = {
+// 		updateSearchTerm: PropTypes.func.isRequired
+// };
--- src/index.js
+++ src/index.js
@@ -6,9 +6,36 @@ import SearchBar from './components/SearchBar'
 
 const App = () => {
 
+	this.updateSearchTerm = (searchTerm) => {
+		console.log(`We are searching for ${searchTerm}`)
+
+		this.getSearchData(searchTerm).then((data) => {
+			console.log(data);
+		})
+	}
+
+	this.getSearchData = async (searchTerm) => {
+		let response, data;
+		try {
+			response = await fetch(`https://data.cityofchicago.org/resource/cwig-ma7x.json?$query=SELECT * where Contains(upper(dba_name), upper("${searchTerm}")) or Contains(upper(aka_name), upper("${searchTerm}"))`)
+			data = await response.json();
+		} catch (e) {
+			console.log(e)
+		}
+		return data
+	};
+
 	return (
 		<div>
-			<SearchBar/>
+			<div style={{
+				display: "flex",
+				justifyContent: "center"
+			}}>
+			<h1>Restaurant Search</h1>
+			</div>
+			<SearchBar
+				updateSearchTerm={this.updateSearchTerm}
+			/>
 		</div>
 	);
 };
